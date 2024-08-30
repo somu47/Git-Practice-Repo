f837ad8d003521ea7fe5f29537d6a1b6b453dc8a
somu47 <somasundarams720@gmail.com>
2024-08-30 11:41:04 +0530

    first

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/Controller/SampleController.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/Controller/SampleController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController{
-	@GetMapping("/")
+	@GetMapping("/yoga")
 	public String getMessage() {
-		return "Hello sec";
+		return "Hello yogaaaaa";
 	}
 	
 	
